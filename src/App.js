4c74d922058217d717ef46eb79ab77e66193d6ef
Teguh Firmansyah <firmansyahtgh13@gmail.com>
2023-03-26 00:56:34 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Beranda from "./components/Beranda";
+import ManajemenBuku from "./components/ManajemenBuku";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" exact element={<Beranda />} />
+
+          <Route path="/manajemen-buku" exact element={<ManajemenBuku />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
